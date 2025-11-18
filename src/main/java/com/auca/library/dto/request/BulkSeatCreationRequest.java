3f027c9bb0bd0d026ca0fa9bbb39988aea380a2e
uwclement <uwumuclement@gmail.com>
2025-11-18 09:46:07 +0200

    added location not null in request

--- src/main/java/com/auca/library/dto/request/BulkSeatCreationRequest.java
+++ src/main/java/com/auca/library/dto/request/BulkSeatCreationRequest.java
@@ -4,6 +4,8 @@ package com.auca.library.dto.request;
 import com.auca.library.model.Location;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotNull;
+
 
 @Data
 public class BulkSeatCreationRequest {
@@ -12,7 +14,8 @@ public class BulkSeatCreationRequest {
     private Integer endNumber;              // e.g., 50
     private String zoneType;                // e.g., "SILENT", "COLLABORATION"
     private Boolean hasDesktop;             // true/false
-    private String description;             // Optional description
+    private String description; 
+    @NotNull(message = "Location is required")            // Optional description
     private Location location;              // GISHUSHU, MASORO
     private Integer floar; 
 }
